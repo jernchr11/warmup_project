d4cacd8f88dd595177828561c948747a00a97e1c
saasbook <saasbook@saasbook.(none)>
2014-02-14 00:04:06 -0800

    init

--- web.js
+++ web.js
@@ -13,7 +13,7 @@ app.get("/", function(req,res) {
     res.set('Content-Type', 'text/plain');
     res.send("Hi :P");
     console.log("TRYING SOMETHING");
-    var query = connection.query("SELECT username FROM users where username = 'a'");
+    var query = connection.query("SELECT username FROM users where username = 'c'");
     query.on('row', function(row) {
 	console.log(row);
 	//res.send(row.username);
