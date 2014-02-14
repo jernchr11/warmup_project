a78d3e1395e08417cd95513ad6abfebddc54038b
saasbook <saasbook@saasbook.(none)>
2014-02-13 22:12:57 -0800

    init

--- web.js
+++ web.js
@@ -2,9 +2,9 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
-//var pg = require('pg');
-//var connection = new pg.Client(process.env.DATABASE_URL);
-//connection.connect();
+var pg = require('pg');
+var connection = new pg.Client(process.env.DATABASE_URL);
+connection.connect();
 app.configure(function(){ app.use(express.bodyParser()); app.use(app.router); });
 
 app.use(logfmt.requestLogger());
@@ -12,16 +12,23 @@ app.use(logfmt.requestLogger());
 app.get("/", function(req,res) {
     res.set('Content-Type', 'text/plain');
     res.send("Hi :D");
-    /*
+    
     connection.query("create table users ( user TEXT primary key, password TEXT, count integer)", function(err) {
-    console.log(err);
-    connection.query("insert into users(user, password, integer) values ('a', 'b', 1)", function(err) {
-        console.log(err);
-        res.end("FINISHED");
-    }
-            );
+	console.log(err);
+	connection.query("insert into users(user, password, integer) values ('a', 'b', 1)", function(err) {
+	    console.log(err);
+	    res.write("FINISHED");
+	    var query = client.query("SELECT * FROM users");
+	    query.on('row', function(row) {
+		res.send(row);
+	    });
+
+	    query.on('end', function() { 
+		client.end();
+		res.end("DONE FOR GOOD");
+	    });
+	});
     });
-    */
 });
 
 app.post("/users/login", function(req, res) {
