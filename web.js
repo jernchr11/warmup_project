c79a176bdc971ba1cf047c5adf31a5f5bbf31540
saasbook <saasbook@saasbook.(none)>
2014-02-13 22:44:28 -0800

    init

--- web.js
+++ web.js
@@ -14,7 +14,7 @@ app.get("/", function(req,res) {
     res.send("Hi :P");
     console.log("TRYING SOMETHING");
     
-    connection.query("CREATE TABLE users ( user varchar(128), password varchar(128), count INTEGER, PRIMARY KEY(user))", function(err) {
+    connection.query("CREATE TABLE userblah ( user varchar(128), PRIMARY KEY(user))", function(err) {
 	console.log("MYERROR: "+err);
 	console.log(err == null);
 	/*
