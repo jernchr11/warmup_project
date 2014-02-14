950ef447cd84dd36bfae716a70fde2f27e1f6af8
saasbook <saasbook@saasbook.(none)>
2014-02-14 00:36:46 -0800

    init

--- web.js
+++ web.js
@@ -3,6 +3,17 @@ var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
 var pg = require('pg');
+
+
+var SUCCESS               =   1;
+var ERR_BAD_CREDENTIALS   =  -1;
+var ERR_USER_EXISTS       =  -2;
+var ERR_BAD_USERNAME      =  -3;
+var ERR_BAD_PASSWORD      =  -4;
+var MAX_USERNAME_LENGTH = 128;
+var MAX_PASSWORD_LENGTH = 128;
+
+
 var connection = new pg.Client(process.env.DATABASE_URL);
 connection.connect();
 app.configure(function(){ app.use(express.bodyParser()); app.use(app.router); });
