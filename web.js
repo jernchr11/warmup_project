5abf589cfb3a1ae3be7d7f5a182392d08ba17ea7
saasbook <saasbook@saasbook.(none)>
2014-02-14 02:00:57 -0800

    init

--- web.js
+++ web.js
@@ -52,6 +52,12 @@ app.post("/TESTAPI/resetFixture", function(req, res) {
     });
 });
 
+app.post("/TESTAPI/unitTests", function(req, res) {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    var jsonResponse = { 'nrFailed' : 0, 'output': "Success", 'totalTests': 10 };
+    res.send(JSON.stringify(jsonResponse));
+}
+
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
